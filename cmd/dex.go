aead66cbdbca02749816b6a950e727814936e8ca
sabooboo <74437615+Sabooboo@users.noreply.github.com>
2022-08-17 16:35:05 -0300

    Add WIP implementation of dex cmmand
    The boolean flags do not work yet.

---
+++ cmd/dex.go
@@ -0,0 +1,95 @@
+/*
+Copyright © 2022 NAME HERE <EMAIL ADDRESS>
+*/
+package cmd
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/Sabooboo/pokecli/dex"
+	"github.com/spf13/cobra"
+)
+
+// dexCmd represents the dex command
+var dexCmd = &cobra.Command{
+	Use:   "dex",
+	Short: "Retrieve all Pokemon from the national or any regional dex.",
+	Long: `This command will retrieve a list of newline-separated Pokemon names from
+the Pokedex matching the specified id.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		// id, default 1
+		id, err := cmd.PersistentFlags().GetInt("id")
+		if id < 1 || err != nil {
+			cmd.Help()
+			return
+		}
+
+		// Only use cache, default no
+		cacheOnly := false
+		cacheOnly, _ = cmd.PersistentFlags().GetBool("cache-only")
+		fmt.Println(cacheOnly)
+
+		// override cache, default no
+		override := false
+		override, _ = cmd.PersistentFlags().GetBool("override")
+
+		var pkdx dex.Pokedex
+
+		if !override {
+			fmt.Println("Override", override)
+			pkdx, err = dex.GetPokedex(dex.ID(id))
+			if err != nil {
+				fmt.Println(err)
+				return
+			}
+			printPokedex(pkdx)
+			return
+		}
+
+		pkdx, err = dex.GetPokedexFromCache(dex.ID(id))
+		if err != nil && cacheOnly {
+			fmt.Println("CacheOnly", cacheOnly)
+			pkdx, err = dex.FetchPokedex(dex.ID(id))
+		}
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		printPokedex(pkdx)
+	},
+}
+
+func printPokedex(pkdx dex.Pokedex) {
+	s := strings.Builder{}
+	for _, v := range pkdx.Names {
+		s.WriteString(fmt.Sprintf("%s\n", v))
+	}
+
+	fmt.Println(s.String())
+}
+
+func init() {
+	rootCmd.AddCommand(dexCmd)
+
+	dexCmd.PersistentFlags().IntP(
+		"id",
+		"i",
+		1,
+		"The id of the pokedex you want to retrieve. 1 retrieves the national dex. Other numbers correspond to generations.",
+	)
+
+	dexCmd.Flags().Bool(
+		"cache-only",
+		false,
+		"Define whether this should only retrieve Pokemon from locally stored pokedex.",
+	)
+
+	dexCmd.Flags().BoolP(
+		"override",
+		"o",
+		false,
+		"Define whether this search the web for the specified dex and override any cached values.",
+	)
+}
