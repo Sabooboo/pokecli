c85ff011ca0515b157ccc6aca1b8c8414a9dc45e
sabooboo <74437615+Sabooboo@users.noreply.github.com>
2022-10-01 08:18:57 -0300

    Add basic file logging capability

--- main.go
+++ main.go
@@ -1,7 +1,24 @@
 package main
 
-import "github.com/Sabooboo/pokecli/cmd"
+import (
+	"fmt"
+	"github.com/Sabooboo/pokecli/cmd"
+	tea "github.com/charmbracelet/bubbletea"
+	"os"
+)
 
 func main() {
+	// TODO Remove explicit Setenv
+	os.Setenv("DEBUG", "yes")
+	if len(os.Getenv("DEBUG")) > 0 {
+		f, err := tea.LogToFile("debug.log", "debug")
+		if err != nil {
+			fmt.Println("fatal:", err)
+			os.Exit(1)
+		}
+		defer f.Close()
+	}
+
+	// Default command is ui, so running app runs all the fancy tui stuff :-)
 	cmd.Execute()
 }
