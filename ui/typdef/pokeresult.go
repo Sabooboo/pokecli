7e0b0661d0e520a4325d06c76ab00505c5da8cfa
Sebastian Dunne <74437615+Sabooboo@users.noreply.github.com>
2022-10-28 23:38:03 -0300

    WIP Stat displaying
    Added very basic display of stats. meta.Data.chart refs statchart component which handles the display of stats. Currently displays upside-down, but need to figure out a whitespace solution to fix this.
    Future reference it is possible to access a range of a slice by referencing the slice[lower:upper], which includes lower but excludes upper, so adding whitespace and then saving the tallest may be the play here.
    Anyways please squash me later

--- ui/typdef/pokeresult.go
+++ ui/typdef/pokeresult.go
@@ -5,12 +5,22 @@ import (
 	"image"
 )
 
+const (
+	Health         = "hp"
+	Attack         = "attack"
+	SpecialAttack  = "special-attack"
+	Defense        = "defense"
+	SpecialDefense = "special-defense"
+	Speed          = "speed"
+)
+
 type PokeResult struct {
 	Pokemon   apistructs.Pokemon
 	Species   apistructs.PokemonSpecies
 	Types     []apistructs.Type
 	Abilities []Ability
 	Images    ShinyToggleable[PokeImg]
+	Stats     Stats[int]
 	Error     error
 }
 
@@ -22,6 +32,15 @@ type ShinyToggleable[T any] struct {
 type Ability struct {
 	Info     apistructs.Ability
 	IsHidden bool
+}
+
+type Stats[T any] struct {
+	Health         T
+	Attack         T
+	SpecialAttack  T
+	Defense        T
+	SpecialDefense T
+	Speed          T
 }
 
 type PokeImg struct {
